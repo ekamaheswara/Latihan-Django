3491f8112be4d7a91aa60b886685c58800266f29
Eka Satria Maheswara <ekamaheswara22@gmail.com>
2021-03-23 00:01:14 +0700

    07. Menggunakan URL pada app

--- django_project/mywebsite/urls.py
+++ django_project/mywebsite/urls.py
@@ -17,16 +17,17 @@ from django.conf.urls import url
 from django.contrib import admin
 # from .views import index, about
 from . import views
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
-# from . import views
-from blog import views as blogViews
+from . import views
+# from blog import views as blogViews
+
 from about import views as aboutViews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/$', blogViews.index),
+    url(r'^blog/', include('blog.urls')),
     url(r'^about/$', aboutViews.index),
     url(r'^$', views.index),
 ]
